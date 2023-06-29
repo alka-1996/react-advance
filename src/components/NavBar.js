60447f9a59a18eeb4876a7254c464a0b40c1936b
anjaliaaru <anjaliaaru994@gmail.com>
2023-06-29 21:20:18 +0530

    all code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,9 +19,9 @@ const NavBar = () => {
                                 <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-primary" type="submit">Search</button>
+                        <form className="d-flex">
+                            <Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
+                            <Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link>
                         </form>
                     </div>
                 </div>
